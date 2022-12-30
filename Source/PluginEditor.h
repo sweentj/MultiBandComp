0f59be9c0ff8688ca2a3276b155b1f8619842d49
sweentj <tj.sweeney@outlook.com>
2022-12-29 20:10:48 -0500

    Added rotary sliders in GlobalControls

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -33,6 +33,82 @@ GUI ROADMAP
 /**
 */
 
+struct LookAndFeel : juce::LookAndFeel_V4
+{
+    void drawRotarySlider(juce::Graphics&,
+        int x, int y, int width, int height,
+        float sliderPosProportional,
+        float rotaryStartAngle,
+        float rotaryEndAngle,
+        juce::Slider&) override;
+
+    void drawToggleButton(juce::Graphics& g,
+        juce::ToggleButton& toggleButton,
+        bool shouldDrawButtonAsHighlighted,
+        bool shouldDrawButtonAsDown) override;
+};
+
+struct RotarySliderWithLabels : juce::Slider
+{
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+        juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+            juce::Slider::TextEntryBoxPosition::NoTextBox),
+        param(&rap),
+        suffix(unitSuffix)
+    {
+        setLookAndFeel(&lnf);
+    }
+
+    ~RotarySliderWithLabels()
+    {
+        setLookAndFeel(nullptr);
+    }
+
+    struct LabelPos
+    {
+        float pos;
+        juce::String label;
+    };
+
+    juce::Array<LabelPos> labels;
+
+    void paint(juce::Graphics& g) override;
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const { return 14; }
+    juce::String getDisplayString() const;
+private:
+    LookAndFeel lnf;
+
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
+};
+
+struct PowerButton : juce::ToggleButton { };
+
+struct AnalyzerButton : juce::ToggleButton
+{
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        auto insetRect = bounds.reduced(4);
+
+        randomPath.clear();
+
+        juce::Random r;
+
+        randomPath.startNewSubPath(insetRect.getX(),
+            insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+
+        for (auto x = insetRect.getX() + 1; x < insetRect.getRight(); x += 2)
+        {
+            randomPath.lineTo(x,
+                insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+        }
+    }
+
+    juce::Path randomPath;
+};
+
 struct PlaceHolder : juce::Component
 {
     PlaceHolder();
@@ -44,6 +120,36 @@ struct PlaceHolder : juce::Component
 
     juce::Colour customColour;
 };
+
+struct RotarySlider : juce::Slider
+{
+    RotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+
+    }
+};
+
+template<typename Attachment, typename APVTS, typename Params, typename ParamName, typename SliderType>
+void makeAttachment(std::unique_ptr<Attachment>& attachment, APVTS& apvts, const Params& params, const ParamName& name, SliderType& slider)
+{
+    attachment = std::make_unique<Attachment>(apvts, params.at(name), slider);
+}
+
+struct GlobalControls : juce::Component
+{
+    GlobalControls(juce::AudioProcessorValueTreeState& apvts);
+
+    void paint(juce::Graphics& g) override;
+
+    void resized() override;
+
+private:
+    RotarySlider inGainSlider, lowMidXoverSlider, midHighXoverSlider, outGainSlider;
+    
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    std::unique_ptr<Attachment> lowMidXoverSliderAttachment, midHighXoverSliderAttachment, inGainSliderAttachment, outGainSliderAttachment;
+};
+
 
 class MultiBandCompAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
@@ -60,7 +166,8 @@ private:
     // access the processor object that created it.
     MultiBandCompAudioProcessor& audioProcessor;
 
-    PlaceHolder controlBar, analyzer, globalControls, bandControls;
+    PlaceHolder controlBar, analyzer, /*globalControls,*/ bandControls;
+    GlobalControls globalControls{ audioProcessor.apvts };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MultiBandCompAudioProcessorEditor)
 };
